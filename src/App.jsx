be10dfcfab26e23c144e747d92e673fe6c46d788
Xjanus12x <70027689+Xjanus12x@users.noreply.github.com>
2024-01-05 15:09:05 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './components/hello'
+import Welcome from './components/welcome'
+
+function App() {
+
+  
+  return (
+    <>
+      <Hello/>
+      <Welcome/>
+    </>
+  )
+}
+
+export default App
